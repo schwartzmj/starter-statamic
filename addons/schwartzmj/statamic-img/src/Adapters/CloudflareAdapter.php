aa8503dd7c5167b90c75396b2971c48d160b1072
Matt Schwartz <19769905+schwartzmj@users.noreply.github.com>
2024-05-13 22:36:07 -0700

    wip

--- addons/schwartzmj/statamic-img/src/Adapters/CloudflareAdapter.php
+++ addons/schwartzmj/statamic-img/src/Adapters/CloudflareAdapter.php
@@ -9,6 +9,6 @@ class CloudflareAdapter implements IAdapter
     public static function toUrl(int $width_to_render, Img $img): string
     {
         $prefix = "/cdn-cgi/image/";
-        return $prefix . "fit=scale-down,width={$width_to_render}" . $img->asset->url();
+        return $prefix . "format=auto,fit=scale-down,width={$width_to_render}" . $img->asset->url();
     }
 }
